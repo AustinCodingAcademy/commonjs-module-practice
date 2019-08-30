776aed368c672dedd287644a1246cc297751a92d
unknown <rmi11s@yahoo.com>
2019-08-29 20:03:27 -0500

    Ryan

--- modules/addNumbers.js
+++ modules/addNumbers.js
@@ -1,9 +1,12 @@
 const add = require("./add");
+module.exports.addOne = function(num) {
+	return add(num, 1);
+};
 
-module.exports.addTen =  function(num) {
-  return add(num,10);
-}
+module.exports.addTen = function(num) {
+	return add(num, 10);
+};
 
 module.exports.addFive = function(num) {
-  return add(num, 5);
-}
+	return add(num, 5);
+};
