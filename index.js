8b0a9a90a23107b768258efdedd04d4350bcc9a0
kevinjj30 <kevinjenkins56@yahoo.com>
2019-10-27 21:10:39 -0500

    extra credit practice

--- index.js
+++ index.js
@@ -3,36 +3,39 @@ const subtract = require("./modules/subtract");
 const multiply= require("./modules/multiply");
 const {addTen, addFive} = require("./modules/addNumbers");
 const {subtractOne}= require("./modules/subtractNumbers");
+const divide= require("./modules/divide");
 
 const number1 = 393;
 const number2 = 238;
 
 let answer = add(number1,number2);
-console.log("Adding numbers",number1,number2,answer);
+console.log("Adding numbers",number1, "+", number2,"=",answer);
 
 answer = subtract(number1,number2);
-console.log(answer);
+console.log("Subtracting numbers", number1, "-", number2,"=",answer);
 
 answer = multiply(number1,number2);
-console.log(answer);
+console.log("Mulitplying", number1, "*", number2, "=", answer);
 
 answer = divide(number1,number2);
-console.log(answer);
+console.log("Dividing", number1, "%", number2, "=", answer);
 
-answer = addTen(number1);
-console.log(answer);
+answer = addTen(number1, 10);
+console.log(number1, "+", 10, "=", answer);
 
 answer = addFive(number1);
-console.log(answer);
+console.log(number1, "+", 5, "=", answer);
 
 answer = addOne(number1);
-console.log(answer);
+console.log(number1, "+", 1, "=", answer);
 
 answer = subtractOne(number1);
-console.log(answer);
+console.log(number1, "-", 1, "=",answer);
 
 answer = subtractFive(number1);
-console.log(answer);
+console.log(number1, "-", 5, "=", answer);
 
 answer = subtractTen(number1);
-console.log(answer);
+console.log(number1, "-", 10, "=", answer);
+
+
