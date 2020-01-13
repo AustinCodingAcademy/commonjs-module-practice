11c5cc5d9d4b26c9723898792081cba2696eb6fd
dgasch512 <gaschdav208@gmail.com>
2020-01-12 22:10:13 -0600

    All calculations return correct value

---
+++ modules/subtractNumbers.js
@@ -0,0 +1,13 @@
+const subtract = require("./subtract");
+
+module.exports.subtractTen =  function(num) {
+  return subtract(num,10);
+}
+
+module.exports.subtractFive = function(num) {
+  return subtract(num, 5);
+}
+
+module.exports.subtractOne = function(num) {
+  return subtract(num, 1);
+}
